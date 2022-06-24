d769836cf2d5f7f829050e988a4eb26315ed3cc0
IMAMSGIT <99043428+IMAMSGIT@users.noreply.github.com>
2022-06-25 00:03:19 +0600

    first commit

---
+++ Getting-started/four-pillars-OOP.js
@@ -0,0 +1,13 @@
+/*
+1.Encapsulation
+2.Abstraction
+3.Inheritance
+4.Polymorphism 
+*/
+
+/*
+Encapsulation---Reduce complexity+increas reusability
+Abstraction---Reduce complexity+isolate impact of changes
+Inheritance---Eliminate redundant code
+Polymorphism---Refactor ugly switch case statements
+*/
---
+++ Getting-started/what-is-oop.js
@@ -0,0 +1,4 @@
+/*
+a programming paradigm centers around
+objects ranter than functions
+*/
